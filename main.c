f6ab62e9fe4d141a57c66cd60ae778a52a36592f
safe1801 <sadinho88@hotmail.com>
2021-02-12 15:39:30 +0100

    Basic C - programming noob.

---
+++ main.c
@@ -0,0 +1,13 @@
+// Single line comment, will not be executed 
+#include<stdio.h>
+
+// This is main function 
+
+int main(){
+    printf("Hello World.\n");
+    return 0;
+}
+
+
+
+
---
+++ main_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/*Multiline comments
+This is a multiline comment
+p
+Multiline can be expanded more than one line as long its inside the scope which is from the first front slash / followed by an asterisk symbol * and enclosed with a second asterisk * and a front slash /.
+Comments are not part of your code
+Comments in program are included to increase the readability.
+There are 2 types of comments in C the  single line comment and the multiline comment 
+ */ 
+
+int main(){
+    printf("Hello World.\n");
+    return 0;
+}
+
+
+
+
