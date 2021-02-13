354ae46340bda3a7b13062afe47cd40f184686ff
safe1801 <sadinho88@hotmail.com>
2021-02-13 01:19:30 +0100

    Escape sequences in C.

---
+++ escape_sequences/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ 
+	/*
+	  Escape sequences are two characters that makes a single character in C. 
+	  \n is for new line 
+	  \t is for horizontal tab.
+	  \' is for single quotes or (apostrophe)
+	  \" is for double quotes.
+	  */	
+	printf("Hello there!\r");
+	printf("Welcome to \"The Complete C course\"");
+	return 0;	
+	
+	
+	} 
